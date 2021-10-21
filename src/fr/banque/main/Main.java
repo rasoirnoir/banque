360e6f029d6bd7174361c1e09946001fa8267552
William Noris <w.noris@protonmail.com>
2021-10-21 14:26:26 +0200

    Liste Operations par compte

--- src/fr/banque/main/Main.java
+++ src/fr/banque/main/Main.java
@@ -1,15 +1,22 @@
 package fr.banque.main;
 
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import fr.banque.models.*;
 import fr.banque.bdd.*;
 
 public class Main {
 
-	public static void main(String[] args) throws ClassNotFoundException,
-	SQLException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
+		Connection co = AccesBD.Connect("banque");
+		
+		
+		Compte c1 = Requetes.getCompteByNumero(10005);
+		System.out.println(c1);
+		
+		co.close();
 	}
 
 }
