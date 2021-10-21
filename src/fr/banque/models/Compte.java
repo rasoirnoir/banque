2bca62b4f579d0dd08276000b772c29db766429b
William Noris <w.noris@protonmail.com>
2021-10-21 10:43:24 +0200

    Compte, Operations

--- src/fr/banque/models/Compte.java
+++ src/fr/banque/models/Compte.java
@@ -2,4 +2,57 @@ package fr.banque.models;
 
 public class Compte {
 
+	private int numero;
+	private TypeCompte typeCompte;
+	private Titulaire titulaire;
+	private float solde;
+	public Compte(int numero, TypeCompte typeCompte, Titulaire titulaire, float solde) {
+		super();
+		this.numero = numero;
+		this.typeCompte = typeCompte;
+		this.titulaire = titulaire;
+		this.solde = solde;
+	}
+	
+	public Compte() {}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public TypeCompte getTypeCompte() {
+		return typeCompte;
+	}
+
+	public void setTypeCompte(TypeCompte typeCompte) {
+		this.typeCompte = typeCompte;
+	}
+
+	public Titulaire getTitulaire() {
+		return titulaire;
+	}
+
+	public void setTitulaire(Titulaire titulaire) {
+		this.titulaire = titulaire;
+	}
+
+	public float getSolde() {
+		return solde;
+	}
+
+	public void setSolde(float solde) {
+		this.solde = solde;
+	}
+
+	@Override
+	public String toString() {
+		return "Compte [numero=" + numero + ", typeCompte=" + typeCompte + ", titulaire=" + titulaire + ", solde="
+				+ solde + "]";
+	}
+	
+	
 }
--- src/fr/banque/models/Operations.java
+++ src/fr/banque/models/Operations.java
@@ -1,5 +1,80 @@
 package fr.banque.models;
 
+import java.sql.Date;
+
 public class Operations {
 
+	private int numero;
+	private Compte compte;
+	private Date date;
+	private String libelle;
+	private float montant;
+	private char typeOp;
+	
+	public Operations() {}
+
+	public Operations(int numero, Compte compte, Date date, String libelle, float montant, char typeOp) {
+		super();
+		this.numero = numero;
+		this.compte = compte;
+		this.date = date;
+		this.libelle = libelle;
+		this.montant = montant;
+		this.typeOp = typeOp;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Compte getCompte() {
+		return compte;
+	}
+
+	public void setCompte(Compte compte) {
+		this.compte = compte;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public float getMontant() {
+		return montant;
+	}
+
+	public void setMontant(float montant) {
+		this.montant = montant;
+	}
+
+	public char getTypeOp() {
+		return typeOp;
+	}
+
+	public void setTypeOp(char typeOp) {
+		this.typeOp = typeOp;
+	}
+
+	@Override
+	public String toString() {
+		return "Operations [numero=" + numero + ", compte=" + compte + ", date=" + date + ", libelle=" + libelle
+				+ ", montant=" + montant + ", typeOp=" + typeOp + "]";
+	}
+	
 }
